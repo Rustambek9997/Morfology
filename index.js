31663b0f1e4782d9fd13e8d811a8ddc4070e3f07
Rustambek Baltayev <rustambek1216@gmail.com>
2021-07-12 20:11:24 +0500

    rename some functions and add db plan image

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const sentencesRepo = new SentencesRepository()
 
 async function main() {
     console.log('All Sentences:')
-    console.log(await sentencesRepo.allSentences())
+    console.log(await sentencesRepo.findAll())
     console.log("get 5th sentence:")
-    console.log(await sentencesRepo.getSentence(5))
+    console.log(await sentencesRepo.findById(5))
 }
 
 main()
--- repository/SentencesRepository.js
+++ repository/SentencesRepository.js
@@ -3,18 +3,23 @@ const db = require('../database')
 const SENTENCE_INSERT = "INSERT INTO Sentences(text) VALUES(?)"
 const SENTENCE_BY_ID = "SELECT * FROM Sentences WHERE ID = ?"
 const SENTENCE_ALL = "SELECT * FROM Sentences"
+const SENTENCE_REMOVE = "DELETE FROM Sentences WHERE ID = ?"
 
 module.exports = class SentencesRepository {
 
-    async addSentence(text) {
+    async insert(text) {
         await db.run(SENTENCE_INSERT, [text])
     }
 
-    async allSentences() {
+    async findAll() {
         return await db.all(SENTENCE_ALL)
     }
 
-    async getSentence(id) {
+    async findById(id) {
         return await db.get(SENTENCE_BY_ID, [id])
+    }
+
+    async remove(id) {
+        await db.run(SENTENCE_REMOVE, [id])
     }
 }
