1839355c544250ddf9f5b49a9f740dc0f71cb4d0
Rustambek Baltayev <rustambek1216@gmail.com>
2021-07-12 19:19:32 +0500

    connect sentences table to repository

--- repository/SentencesRepository.js
+++ repository/SentencesRepository.js
@@ -1,46 +1,20 @@
-const db = require('../database/database')
+const db = require('../database')
 
 const SENTENCE_INSERT = "INSERT INTO Sentences(text) VALUES(?)"
 const SENTENCE_BY_ID = "SELECT * FROM Sentences WHERE ID = ?"
 const SENTENCE_ALL = "SELECT * FROM Sentences"
 
-
-"INSERT | DELETE | UPDATE"
-
 module.exports = class SentencesRepository {
 
-    addSentence(text) {
-        db.run(SENTENCE_INSERT, [text], (err) => {
-            if (err) {
-                console.log("INSERT SENTENCE: " + err.message)
-            }
-        })
+    async addSentence(text) {
+        await db.run(SENTENCE_INSERT, [text])
     }
 
     async allSentences() {
-        const all = new Promise((resolve, reject) => {
-            
-            db.all(SENTENCE_ALL, (err, rows) => {
-                if (err) {
-                    reject(err)
-                }
-                else {
-                    resolve(rows)
-                }
-            })
-
-        })
-        try {
-            const data = await all()
-            return data
-        }
-        catch(err) {
-            console.log(err)
-            return []
-        }
+        return await db.all(SENTENCE_ALL)
     }
 
-    getSentence(id) {
-
+    async getSentence(id) {
+        return await db.get(SENTENCE_BY_ID, [id])
     }
 }
